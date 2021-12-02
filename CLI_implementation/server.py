3dc747bf448f7eabf90a4d49271f7585e2c2189a
Hamza <baniatah@inf.u-szeged.hu>
2021-12-02 14:49:00 +0100

    propagation_modification

--- CLI_implementation/server.py
+++ CLI_implementation/server.py
@@ -32,9 +32,11 @@ def handle_client(connection, address):
         string_header_length = connection.recv(header).decode(FORMAT)
         if string_header_length:
             int_header_length = int(string_header_length)
+            print(int_header_length)
             string_content_length = connection.recv(int_header_length).decode(FORMAT)
             if string_content_length:
                 int_content_length = int(string_content_length)
+                print(int_content_length)
                 content = connection.recv(int_content_length).decode(FORMAT)
                 if content:
                     print(content)
