2a6fc8a2ed7cc3edda8aa33cbded29b2972f5e17
Hamza <baniatah@inf.u-szeged.hu>
2021-12-02 14:16:52 +0100

    propagation_modification

--- CLI_implementation/server.py
+++ CLI_implementation/server.py
@@ -38,7 +38,7 @@ def handle_client(connection, address):
                 content = connection.recv(int_content_length).decode(FORMAT)
                 if content:
                     connection.close()
-                    received_dictionary = json.load(content)
+                    received_dictionary = json.loads(content.read())
                     memory_pool.received_msgs.put([received_dictionary, address, time.time()])
     except Exception as e:
         print(e)
